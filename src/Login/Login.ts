24dad146072cb7fe8c65f43e5490f7bd2357720d
Artemiy <rendrom@gmail.com>
2019-04-04 21:17:27 +0800

    login validate input

--- src/Login/Login.ts
+++ src/Login/Login.ts
@@ -16,6 +16,23 @@ export class Login extends Vue {
   url: string = '';
   login: string = '';
   password: string = '';
+  formHasErrors = true;
+  isLoading = false;
+  loginErrorMessage = '';
+
+  form: string[] = ['url', 'login', 'password'];
+
+  rules: { [x: string]: (value: string) => string | boolean } = {
+    validUrl: (value) => {
+      const isHttp = /^https?/.test(value);
+      if (!isHttp) {
+        return 'Url should start with http:// or https://';
+      }
+      return true;
+      // const isUrlValid = this.isUrlValid(value);
+      // return isUrlValid || 'Url is not valid';
+    }
+  };
 
   get isValid() {
     if (!this.url) {
@@ -26,16 +43,36 @@ export class Login extends Vue {
         return false;
       }
     }
-    return true;
+    return this.form.every((f) => {
+      const input = this.$refs[f];
+      // @ts-ignore
+      return input ? input.valid : true;
+    });
   }
 
   onGoBtnClick() {
-    this.setWebGis({
-      url: this.url,
-      auth: {
-        login: this.login,
-        password: this.password
-      }
-    });
+    if (this.isValid) {
+      this.isLoading = true;
+      this.loginErrorMessage = '';
+      this.setWebGis({
+        url: this.url,
+        auth: {
+          login: this.login,
+          password: this.password
+        }
+      }).then(() => {
+        this.isLoading = false;
+      }).catch((er: any) => {
+        console.log(er);
+        this.loginErrorMessage = 'Login error';
+        this.isLoading = false;
+      });
+    }
+  }
+
+  private isUrlValid(userInput: string) {
+    const res = userInput.match(
+      /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+    return res !== null;
   }
 }
--- src/Login/Login.vue
+++ src/Login/Login.vue
@@ -8,19 +8,34 @@
               <v-toolbar-title>Connext to WebGIS</v-toolbar-title>
             </v-toolbar>
             <v-card-text>
-              <v-form>
-                <v-text-field v-model="url" name="url" label="WebGIS URL" type="text"></v-text-field>
+              <v-form @submit="onGoBtnClick" onSubmit="return false;">
+                <v-text-field
+                  v-model="url"
+                  ref="url"
+                  label="WebGIS URL"
+                  type="text"
+                  :rules="[rules.validUrl]"
+                ></v-text-field>
 
                 <v-switch v-model="guest" label="As guest"></v-switch>
                 <div v-if="!guest">
-                  <v-text-field v-model="login" name="login" label="Login" type="text"></v-text-field>
-                  <v-text-field v-model="password" name="password" label="Password" id="password" type="password"></v-text-field>
+                  <v-text-field v-model="login" ref="login" label="Login" type="text"></v-text-field>
+                  <v-text-field
+                    v-model="password"
+                    ref="password"
+                    label="Password"
+                    id="password"
+                    type="password"
+                  ></v-text-field>
                 </div>
               </v-form>
             </v-card-text>
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn @click="onGoBtnClick" color="primary" :disabled="!isValid">Go</v-btn>
+              <v-btn v-if="isLoading" @click="onGoBtnClick" color="primary" disabled>
+                <v-progress-circular indeterminate></v-progress-circular>
+              </v-btn>
+              <v-btn v-else @click="onGoBtnClick" color="primary" :disabled="!isValid">Go</v-btn>
             </v-card-actions>
           </v-card>
         </v-flex>
